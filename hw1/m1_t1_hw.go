a5bed462a6134bfa2a911bd993296e599c86f8e2
SergeySeydel <60809017+SergeySeydel@users.noreply.github.com>
2024-03-21 00:30:21 +0300

    first problem

---
+++ hw1/m1_t1_hw.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	const costPerLiter = 48 
+
+	var distance, consumption float64
+
+	fmt.Println("Введите расстояние в километрах:")
+	fmt.Scanln(&distance)
+
+	fmt.Println("Введите расход топлива в литрах на 100 км:")
+	fmt.Scanln(&consumption)
+
+	if distance < 50 || distance > 10000 || consumption < 5 || consumption > 25 {
+		fmt.Println("Пожалуйста, введите корректные данные.")
+		return
+	}
+
+	totalCost := (distance / 100) * consumption * costPerLiter
+	fmt.Printf("Стоимость поездки составит: %.2f рублей\n", totalCost)
+}
