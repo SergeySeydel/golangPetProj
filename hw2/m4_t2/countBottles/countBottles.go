e54ffe36a9d129a4295bc5fd37038fc074b24cd7
SergeySeydel <s.filyaev@mail.ru>
2024-03-24 20:05:19 +0300

    improved bottles

--- hw2/m4_t2/countBottles/countBottles.go
+++ hw2/m4_t2/countBottles/countBottles.go
@@ -3,15 +3,14 @@ package countBottles
 
 func CountBottles(a int) string{
 
-	var b int
-	b = a % 10
+	a = a % 10
 
 	switch{
-	case b == 1:
+	case a == 1:
 		return "butilka"
-	case b > 1 && a < 5:
+	case a > 1 && a < 5:
 		return "butilki"
-	case b > 4:
+	case a > 4:
 		return "butilok"
 	default:
 		return "butilok"
