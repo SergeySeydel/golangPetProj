17bd5f828fba0e1390b1fbe71221dd466156c498
SergeySeydel <s.filyaev@mail.ru>
2024-03-24 19:24:23 +0300

    fourth task improved

--- hw2/m4_t4/main.go
+++ hw2/m4_t4/main.go
@@ -2,27 +2,32 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
-	var flag,cycleCounts int
+	var flag, cycleCounts int
 
 	fmt.Print("Enter size of checkmate desk: ")
 	fmt.Scan(&cycleCounts)
 
-	if cycleCounts < 0 || cycleCounts > 20{
+	if cycleCounts < 0 || cycleCounts > 20 {
 		fmt.Println("Error, please enter correct size")
 		return
 	}
 
-	for i := 0; i < cycleCounts; i++{
-		for j := 0; j < cycleCounts;j++{
+	for i := 0; i < cycleCounts; i++ {
+		for j := 0; j < cycleCounts; j++ {
 			fmt.Printf("%d ", flag)
-			if flag{
+			if flag == 0 {
+				flag = 1
+			} else {
 				flag = 0
 			}
-			else{
-				flag = 1
-			}
 		}
-		fmt.Print("\n")
+		fmt.Println()
+		if i % 2 == 1{
+			flag = 0
+		} else{
+			flag = 1
+		}
 	}
+}
